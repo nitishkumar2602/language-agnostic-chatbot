fffd1f7aee2f2ad46a36b4bad62f4cfda0a97671
rtk-rnjn <ritik0ranjan+github@gmail.com>
2025-09-11 22:10:47 +0530

    Refactor authentication routes and update links in templates to use Flask's url_for

--- src/routes/authentication.py
+++ src/routes/authentication.py
@@ -5,12 +5,12 @@ from ..app import app
 __all__ = ("register", "login", "login_post", "register_post")
 
 
-@app.get("/login-register/")
+@app.get("/register/")
 def register():
     return render_template("login-register.jinja", active_tab="register")
 
 
-@app.get("/login-register/")
+@app.get("/login/")
 def login():
     return render_template("login-register.jinja", active_tab="login")
 
